f97d27d5dac16ad988fd2285da920af0bd4b07aa
Artemisa Nuri <artenisa.nuri@live.com>
2024-07-25 12:53:49 +0200

    Header fixes

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,47 +1,60 @@
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import React, { useState } from 'react';
+import EmailIcon from '@mui/icons-material/Email';
+import PersonIcon from '@mui/icons-material/Person';
 import SettingsIcon from '@mui/icons-material/Settings';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import React from 'react';
-import codeviderLogo from '/Images/codevider.png'
-import style from './header.module.css'
-
+import LogoutIcon from '@mui/icons-material/Logout';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import codeviderLogo from '/Images/codevider.png';
+import style from './header.module.css';
 
 const Header: React.FC<{ isOpen: boolean }> = ({ isOpen }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <header className={style.header}>
       <div className={style.headerLeft}>
         <div className={style.logo}>
-          <img alt='logo' src={codeviderLogo}  />
+          <img alt='logo' src={codeviderLogo} />
         </div>
         {isOpen && (
           <>
-        <div className={style.title}>
-        <h4>code</h4>
-        </div>
-        <div className={style.title1}>
-        <h4>vider</h4>
-        </div>
-        </>
+            <div className={style.title}>
+              <h4>code</h4>
+            </div>
+            <div className={style.title1}>
+              <h4>vider</h4>
+            </div>
+          </>
         )}
       </div>
       <div className={style.headerRight}>
         <div className={style.icon}>
-          <NotificationsIcon />
+          <EmailIcon />
           <span className={style.badge}>3</span>
         </div>
-        <div className={style.icon}>
+        <div className={style.icon} onClick={toggleDropdown}>
+          <PersonIcon />
+          <div className={style.username}></div>
+          {showDropdown && (
+            <div className={style.dropdown}>
+              <div className={style.dropdownItem} onClick={() => console.log('View Profile')}>Profile <PermIdentityIcon/></div>
+              <div className={style.dropdownItem} onClick={() => console.log('Logout')}>Logout <LogoutIcon/></div>
+              <div className={style.dropdownItem} onClick={() => console.log('Settings')}>Settings <SettingsIcon />
+              </div>
+
+            </div>
+          )}
+        </div>
+        {/* <div className={style.icon}>
           <SettingsIcon />
-        </div>
-        <div className={style.icon}>
-          <AccountCircleIcon />
-          <span className={style.username}>Profile</span>
-        </div>
+        </div> */}
       </div>
     </header>
   );
 }
 
 export default Header;
-
-
-
