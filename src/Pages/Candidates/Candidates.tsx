8fa314664855784c51e90ac3ff441d997a477cbe
GertKadiu <gertikadiu56@gmail.com>
2024-07-25 11:44:49 +0200

    Reusable table

--- src/Pages/Candidates/Candidates.tsx
+++ src/Pages/Candidates/Candidates.tsx
@@ -1,9 +1,138 @@
+import { Link } from "react-router-dom";
+import DataTable from "../../Components/Table/Table";
+import style from "./styles/Candidates.module.css"
+import { GridRenderCellParams } from '@mui/x-data-grid'
+
+export interface CandidateRow {
+  id: number;
+  originalId: string;
+  firstName: string;
+  role: string;
+  phone: string;
+  lastName: string;
+  email: string | undefined;
+  position: string;
+  date: string;
+  fullName: string;
+}
+
+const columns = [
+  { field: '_id', headerName: 'ID', width: 100 },
+  { field: 'firstName', headerName: 'First Name', width: 100 },
+  { field: 'lastName', headerName: 'Last Name', width: 100 },
+  { field: 'email', headerName: 'Email', width: 200 },
+  { field: 'phone', headerName: 'Phone', width: 150 },
+  { field: 'position', headerName: 'Position', width: 250 },
+  { field: 'date', headerName: 'Date', width: 100 },
+  { field: 'actions', headerName: 'Actions', width: 150 },
+]
+
+const dummyUsers = [
+  {
+    _id: '1a2b3c',
+    firstName: 'John',
+    lastName: 'Doe',
+    role: 'Admin',
+    phone: '123-456-7890',
+    position:"Senior Frontend Developer",
+    date:"22/01/2024",
+    auth: {
+      email: 'john.doe@example.com'
+    }
+  },
+  {
+    _id: '4d5e6f',
+    firstName: 'Jane',
+    lastName: 'Smith',
+    role: 'User',
+    phone: '987-654-3210',
+    position:"Senior Backend Developer",
+    date:"22/03/2024",
+    auth: {
+      email: 'jane.smith@example.com'
+    }
+  },
+  {
+    _id: '7g8h9i',
+    firstName: 'Alice',
+    lastName: 'Johnson',
+    role: 'Editor',
+    phone: '555-123-4567',
+    position:"Junior Backend Developer",
+    date:"22/05/2024",
+    auth: {
+      email: 'alice.johnson@example.com'
+    }
+  },
+  {
+    _id: '10j11k',
+    firstName: 'Bob',
+    lastName: 'Brown',
+    role: 'Admin',
+    phone: '444-555-6666',
+    position:"Senior Backend Developer",
+    date:"22/06/2024",
+    auth: {
+      email: 'bob.brown@example.com'
+    }
+  },
+  {
+    _id: '11k',
+    firstName: 'Bobo',
+    lastName: 'Brown',
+    role: 'Admin',
+    phone: '444-555-6666',
+    position:"Senior Backend Developer",
+    date:"22/06/2024",
+    auth: {
+      email: 'bob.brown@example.com'
+    }
+  },  {
+    _id: '112k',
+    firstName: 'Bobo2',
+    lastName: 'Brown2',
+    role: 'Admin2',
+    phone: '444-555-6666',
+    position:"Senior Backend Developer",
+    date:"22/06/2024",
+    auth: {
+      email: 'bob.brown@example.com'
+    }
+  },
+  { 
+    field: 'actions', 
+    headerName: 'Actions', 
+    width: 120,
+    renderCell: (params: GridRenderCellParams) => (
+        <Link style={{textDecoration:"none", color:"#4C556B"}} to={`/profile/${params.row.originalId}`}>
+        View More
+      </Link>
+    ),
+},
+  
+];
 
 
+
+const rows = dummyUsers.map((user, index) => ({
+  id: index + 1,
+  originalId: user._id,
+  firstName: user.firstName,
+  role: user.role,
+  phone: user.phone,
+  lastName: user.lastName,
+  email: user.auth?.email,
+  position: user.position,
+  date: user.date,
+  fullName: `${user.firstName} ${user.lastName}`
+}));
+const getRowId = (row: CandidateRow ) => row.id;
 export default function Candidates() {
   return (
-    <div>
-      Candidates
+    <div className={style.content}>
+        <div className={style.Candidates}>Candidates</div>
+      <DataTable getRowId={getRowId} columns={columns} rows={rows}  />
     </div>
+
   )
 }
--- src/Pages/Employees/Employees.tsx
+++ src/Pages/Employees/Employees.tsx
@@ -1,6 +1,20 @@
 import { useGetAllUsers } from "./Hook";
 import style from './Employees.module.css'
 import DataTable from "../../Components/Table/Table";
+import { GridRenderCellParams } from '@mui/x-data-grid'
+import { Link } from "react-router-dom";
+import { Monitor, Email, Person, Work } from "@mui/icons-material";
+
+interface EmployeeRow {
+    id: number;
+    originalId: number;
+    firstName: string;
+    role: string;
+    phone: string;
+    lastName: string;
+    email: string | undefined;
+    fullName: string;
+}
 
 export default function Employees() {
     const { users } = useGetAllUsers();
@@ -18,21 +32,50 @@ export default function Employees() {
 
     const columns = [
         { field: 'id', headerName: 'No', width: 80 },
-        { field: 'firstName', headerName: 'First name', width: 150 },
-        { field: 'lastName', headerName: 'Last name', width: 150 },
-        { field: 'email', headerName: 'Email', width: 250 },
-        { field: 'phone', headerName: 'Phone', width: 250 },
-        { field: 'role', headerName: 'Role', width: 150 },
+        { field: 'firstName', headerName: 'First name', width: 100 },
+        { field: 'lastName', headerName: 'Last name', width: 100 },
+        { field: 'email', headerName: 'Email', width: 150 },
+        { field: 'phone', headerName: 'Phone', width: 200 },
+        { field: 'role', headerName: 'Role', width: 100 },
         { field: 'fullName', headerName: 'Full Name', width: 200 },
+        { 
+            field: 'actions', 
+            headerName: 'Actions', 
+            width: 120,
+            renderCell: (params: GridRenderCellParams) => (
+                <Link style={{textDecoration:"none", color:"#4C556B"}} to={`/profile/${params.row.originalId}`}>
+                View More
+              </Link>
+            ),
+        },
     ];
 
-    const getRowId = (row ) => row.id;
+    // const headerIcons = {
+    //     email: Email,
+    //     phone: Monitor,
+    //     role: Work,
+    //     fullName: Person,
+    // };
+
+    const getRowId = (row: EmployeeRow) => row.id;
 
     return (
         <>
             <div style={{ display: "flex", width: "100%", flexDirection: "column", padding: "0 16px", backgroundColor: "#f0f5ff" }}>
-                <div className={style.account}>Employe List</div>
-                <DataTable rows={rows} columns={columns} getRowId={getRowId} />
+                <div className={style.account}>Employee List</div>
+                <DataTable 
+                    rows={rows} 
+                    columns={columns} 
+                    getRowId={getRowId} 
+                    headerIcons={
+                        {
+                            email: Email,
+                            phone: Monitor,
+                            role: Work,
+                            fullName: Person,
+                        }
+                    }
+                />
             </div>
         </>
     );
