b14eea180827dd547d2acb58f081736732617f65
Vasjan Çupri <vasjancupri04@gmail.com>
2024-07-31 12:34:34 +0200

    Added employees and their assets

--- src/Pages/Interview/Interview.tsx
+++ src/Pages/Interview/Interview.tsx
@@ -1,14 +1,14 @@
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
-import HistoryIcon from '@mui/icons-material/History';
-import { GridRenderCellParams } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import HistoryIcon from "@mui/icons-material/History";
+import { GridRenderCellParams } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
 
-import Button from '../../Components/Button/Button';
-import { ButtonTypes } from '../../Components/Button/ButtonTypes';
-import DataTable from '../../Components/Table/Table'
-import RescheduleModal from './Component/scheduleForm';
-import style from './styles/Interview.module.css'
+import Button from "../../Components/Button/Button";
+import { ButtonTypes } from "../../Components/Button/ButtonTypes";
+import DataTable from "../../Components/Table/Table";
+import RescheduleModal from "./Component/ScheduleForm";
+import style from "./styles/Interview.module.css";
 
 interface Interview {
   fullName: string;
@@ -19,7 +19,7 @@ interface Interview {
   time: string;
   cvAttachment: string;
   notes: string;
-  phase:string;
+  phase: string;
 }
 
 interface RowData {
@@ -58,7 +58,6 @@ const dummyInterviews: Interview[] = [
     cvAttachment: "gerti_kadiu_cv.pdf",
     notes: "interview scheduled",
     phase: "First Interview",
-
   },
   {
     fullName: "Redi Balla",
@@ -70,7 +69,6 @@ const dummyInterviews: Interview[] = [
     cvAttachment: "redi_balla_cv.pdf",
     notes: "interview scheduled",
     phase: "First Interview",
-
   },
   {
     fullName: "Vasjan Cupri",
@@ -82,7 +80,6 @@ const dummyInterviews: Interview[] = [
     cvAttachment: "vasjan_cupri_cv.pdf",
     notes: "interview scheduled",
     phase: "First Interview",
-
   },
   {
     fullName: "Selma Bakiu",
@@ -94,7 +91,6 @@ const dummyInterviews: Interview[] = [
     cvAttachment: "selma_bakiu_cv.pdf",
     notes: "interview scheduled",
     phase: "First Interview",
-
   },
   {
     fullName: "Gerald Bane",
@@ -106,31 +102,20 @@ const dummyInterviews: Interview[] = [
     cvAttachment: "gerald_bane_cv.pdf",
     notes: " Great skills ",
     phase: "First Interview",
-
   },
 ];
 
 export default function Interview() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedInterview, setSelectedInterview] = useState<Interview | null>(null);
+  const [selectedInterview, setSelectedInterview] = useState<Interview | null>(
+    null,
+  );
 
   const [interviews, setInterviews] = useState<Interview[]>(dummyInterviews);
   const [isReschedule, setIsReschedule] = useState(false);
 
-  const [rows, setRows] = useState<RowData[]>(interviews.map((interview, index) => ({
-    id: index + 1,
-    fullName: interview.fullName,
-    email: interview.auth?.email,
-    phone: interview.phone,
-    position: interview.position,
-    date: interview.date,
-    time: interview.time,
-    cvAttachment: interview.cvAttachment,
-    notes: interview.notes,
-    phase: interview.phase,
-  })));
-  useEffect(() => {
-    setRows(interviews.map((interview, index) => ({
+  const [rows, setRows] = useState<RowData[]>(
+    interviews.map((interview, index) => ({
       id: index + 1,
       fullName: interview.fullName,
       email: interview.auth?.email,
@@ -141,15 +126,32 @@ export default function Interview() {
       cvAttachment: interview.cvAttachment,
       notes: interview.notes,
       phase: interview.phase,
-
-    })));
+    })),
+  );
+  useEffect(() => {
+    setRows(
+      interviews.map((interview, index) => ({
+        id: index + 1,
+        fullName: interview.fullName,
+        email: interview.auth?.email,
+        phone: interview.phone,
+        position: interview.position,
+        date: interview.date,
+        time: interview.time,
+        cvAttachment: interview.cvAttachment,
+        notes: interview.notes,
+        phase: interview.phase,
+      })),
+    );
   }, [interviews]);
 
-  const handleOpenModal = (interview: Interview, isReschedule: boolean = false) => {
+  const handleOpenModal = (
+    interview: Interview,
+    isReschedule: boolean = false,
+  ) => {
     setSelectedInterview(interview);
     setIsModalOpen(true);
     setIsReschedule(isReschedule);
-
   };
 
   const handleCloseModal = () => {
@@ -159,19 +161,28 @@ export default function Interview() {
 
   const handleReschedule = (date: string, time: string) => {
     if (selectedInterview) {
-      const updatedInterviews = interviews.map(interview => 
+      const updatedInterviews = interviews.map((interview) =>
         interview.fullName === selectedInterview.fullName
-        ? { ...interview, date, time, phase: isReschedule ? interview.phase : 'Second Interview' }
-        : interview
+          ? {
+              ...interview,
+              date,
+              time,
+              phase: isReschedule ? interview.phase : "Second Interview",
+            }
+          : interview,
       );
       setInterviews(updatedInterviews);
     }
     handleCloseModal();
   };
   const handleCancel = (interview: Interview) => {
-    const isConfirmed = window.confirm(`Are you sure you want to cancel the interview with ${interview.fullName}?`);
+    const isConfirmed = window.confirm(
+      `Are you sure you want to cancel the interview with ${interview.fullName}?`,
+    );
     if (isConfirmed) {
-      const updatedInterviews = interviews.filter(i => i.fullName !== interview.fullName);
+      const updatedInterviews = interviews.filter(
+        (i) => i.fullName !== interview.fullName,
+      );
       setInterviews(updatedInterviews);
       handleCloseModal();
     }
@@ -180,78 +191,101 @@ export default function Interview() {
   const handleAccept = (interview: Interview) => {
     handleOpenModal(interview, false);
   };
-  
 
   const columns = [
-    { field: 'id', headerName: 'No', width: 20 },
-    { field: 'fullName', headerName: ' Name', flex:1 },
-    { field: 'email', headerName: 'Email', flex:1 },
-    { field: 'phone', headerName: 'Phone', flex:1 },
-    { field: 'position', headerName: 'Position', flex:1 },
-    { field: 'date', headerName: 'Date' ,flex:1},
-    { field: 'time', headerName: 'Time', width:60  },
-    { field: 'cvAttachment', headerName: 'CV', flex:1 },
-    { field: 'phase', headerName: 'Phase', flex:1  },
-    {field: 'notes', headerName: 'Notes', flex:1  },
+    { field: "id", headerName: "No", width: 20 },
+    { field: "fullName", headerName: " Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "phone", headerName: "Phone", flex: 1 },
+    { field: "position", headerName: "Position", flex: 1 },
+    { field: "date", headerName: "Date", flex: 1 },
+    { field: "time", headerName: "Time", width: 60 },
+    { field: "cvAttachment", headerName: "CV", flex: 1 },
+    { field: "phase", headerName: "Phase", flex: 1 },
+    { field: "notes", headerName: "Notes", flex: 1 },
     {
-      field: 'reschedule',
-      headerName: 'Reschedule',
+      field: "reschedule",
+      headerName: "Reschedule",
       width: 120,
       renderCell: (params: GridRenderCellParams) => (
-        <div style={{ display: "flex", justifyContent: "center" , gap:"5px", paddingTop:"10px"}}>
-
-        <Button 
-          type={ButtonTypes.PRIMARY}
-          btnText=""
-          width="60px"
-          height="30px"
-          display='flex'
-          justifyContent='center'
-          alignItems='center'
-          icon={<HistoryIcon />}
-      
-          onClick={() => handleOpenModal(params.row as Interview)}
-        />
-</div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "5px",
+            paddingTop: "10px",
+          }}
+        >
+          <Button
+            type={ButtonTypes.PRIMARY}
+            btnText=""
+            width="60px"
+            height="30px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            icon={<HistoryIcon />}
+            onClick={() => handleOpenModal(params.row as Interview)}
+          />
+        </div>
       ),
     },
-    {field : 'manage', headerName: 'Manage', width: 120, renderCell: (params: GridRenderCellParams)=>(
-     <div style={{ display: "flex", justifyContent: "center" , gap:"5px", paddingTop:"10px"}}>
-      <Button
-      btnText=''
-        type={ButtonTypes.PRIMARY}
-        width="60px"
-        height="30px"
-        backgroundColor='#40a829'
-        borderColor='#40a829'
-        display='flex'
-        justifyContent='center'
-        alignItems='center'
-        icon={<CheckIcon />}
-        onClick={() => handleAccept(params.row as Interview)}
-></Button>
-<Button
-      btnText=''
-        type={ButtonTypes.PRIMARY}
-        width="60px"
-        height="30px"
-        backgroundColor='#C70039'
-        borderColor='#C70039'
-        display='flex'
-        justifyContent='center'
-        alignItems='center'
-        icon={<CloseIcon />}
-        onClick={() => handleCancel(params.row as Interview)}>
-</Button>
-
-</div>
-    )}
+    {
+      field: "manage",
+      headerName: "Manage",
+      width: 120,
+      renderCell: (params: GridRenderCellParams) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "5px",
+            paddingTop: "10px",
+          }}
+        >
+          <Button
+            btnText=""
+            type={ButtonTypes.PRIMARY}
+            width="60px"
+            height="30px"
+            backgroundColor="#40a829"
+            borderColor="#40a829"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            icon={<CheckIcon />}
+            onClick={() => handleAccept(params.row as Interview)}
+          ></Button>
+          <Button
+            btnText=""
+            type={ButtonTypes.PRIMARY}
+            width="60px"
+            height="30px"
+            backgroundColor="#C70039"
+            borderColor="#C70039"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            icon={<CloseIcon />}
+            onClick={() => handleCancel(params.row as Interview)}
+          ></Button>
+        </div>
+      ),
+    },
   ];
 
   const getRowId = (row: RowData) => row.id;
 
   return (
-    <div style={{ display: "flex", width: "100%", flexDirection: "column", padding: "0 16px", backgroundColor: "#f0f5ff" }}>
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        flexDirection: "column",
+        padding: "0 16px",
+        backgroundColor: "#f0f5ff",
+      }}
+    >
       <div className={style.title}>Interviews</div>
       <DataTable rows={rows} columns={columns} getRowId={getRowId} />
       {selectedInterview && (
@@ -260,8 +294,10 @@ export default function Interview() {
           handleClose={handleCloseModal}
           handleReschedule={handleReschedule}
           selectedInterview={selectedInterview}
-          handleCancel={() => selectedInterview && handleCancel(selectedInterview)}
-          />
+          handleCancel={() =>
+            selectedInterview && handleCancel(selectedInterview)
+          }
+        />
       )}
     </div>
   );
