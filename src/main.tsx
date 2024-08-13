9b613cc66cac6a24278fe8b15551ecd76d3e6ee2
Vasjan Çupri <vasjancupri04@gmail.com>
2024-08-13 11:00:40 +0200

    added a default refetcher for every element that uses tannstack query

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.scss'
 
-const queryClient = new QueryClient()
+
+
+const queryClient = new QueryClient({
+defaultOptions: {
+  queries: {
+    refetchInterval: 1000 * 60 * 1,
+    
+  }
+}
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
