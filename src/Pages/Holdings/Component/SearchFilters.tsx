e46a000f2a1aaae1c00dc6851d1467bfd4b01016
Vasjan Çupri <vasjancupri04@gmail.com>
2024-09-09 16:10:52 +0200

    Fixed some UI errors and a validation error

--- src/Pages/Holdings/Component/SearchFilters.tsx
+++ src/Pages/Holdings/Component/SearchFilters.tsx
@@ -11,10 +11,22 @@ import {
     Box,
     radioClasses,
 } from '@mui/joy'
+import { CssVarsProvider, extendTheme } from '@mui/joy/styles'
 import { TooltipImproved } from '@/Components/Tooltip/Tooltip'
 
 export const HoldingsSearchFilter = () => {
     const { searchParams, setSearchParams } = useContext(HoldingsContext)
+    const theme = extendTheme({
+        components: {
+            JoyRadioGroup: {
+                styleOverrides: {
+                    root: {
+                        '--Radio-radius': '7.5px',
+                    },
+                },
+            },
+        },
+    })
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearchParams((prev) => {
             const value = e.target.value
@@ -70,93 +82,96 @@ export const HoldingsSearchFilter = () => {
                 initialValue={searchParams.get('search') || ''}
                 onChange={onSearchChange}
             />
-            <Box sx={{ display: 'flex', gap: 2 }}>
-                <FormLabel
-                    id="filter-user-choices"
-                    sx={{
-                        fontWeight: 'xl',
-                        textTransform: 'uppercase',
-                        fontSize: 'xs',
-                        letterSpacing: '0.1em',
-                    }}
-                >
-                    Filter by
-                </FormLabel>
-                <RadioGroup
-                    aria-labelledby="product-size-attribute"
-                    defaultValue={
-                        searchParams.get('users')
-                            ? searchParams.get('users')
-                            : 'all'
-                    }
-                    sx={{
-                        gap: 1,
+            <CssVarsProvider theme={theme}>
+                <Box sx={{ display: 'flex', gap: 2 }}>
+                    <FormLabel
+                        id="filter-user-choices"
+                        sx={{
+                            fontWeight: 'xl',
+                            textTransform: 'uppercase',
+                            fontSize: 'xs',
+                            letterSpacing: '0.1em',
+                        }}
+                    >
+                        Filter by
+                    </FormLabel>
+                    <RadioGroup
+                        aria-labelledby="product-size-attribute"
+                        defaultValue={
+                            searchParams.get('users')
+                                ? searchParams.get('users')
+                                : 'all'
+                        }
+                        sx={{
+                            gap: 1,
 
-                        flexWrap: 'wrap',
-                        flexDirection: 'row',
-                    }}
-                    onChange={handleChange}
-                >
-                    {userFilterChoices.map((usersFilter) => (
-                        <Sheet
-                            key={usersFilter}
-                            sx={{
-                                position: 'relative',
-                                height: 40,
-                                flexShrink: 0,
-                                padding: '0.333rem 1rem',
-                                display: 'flex',
-                                alignItems: 'center',
-                                alignSelf: 'end',
-                                borderRadius: 7.5,
-                                justifyContent: 'center',
-                                '--joy-focus-outlineOffset': '4px',
-                                '--joy-palette-focusVisible': (theme) =>
-                                    theme.vars.palette.neutral.outlinedBorder,
-                                [`& .${radioClasses.checked}`]: {
-                                    [`& .${radioClasses.label}`]: {
-                                        fontWeight: 'lg',
+                            flexWrap: 'wrap',
+                            flexDirection: 'row',
+                        }}
+                        onChange={handleChange}
+                    >
+                        {userFilterChoices.map((usersFilter) => (
+                            <Sheet
+                                key={usersFilter}
+                                sx={{
+                                    position: 'relative',
+                                    height: 40,
+                                    flexShrink: 0,
+                                    padding: '0.333rem 1rem',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    alignSelf: 'end',
+                                    borderRadius: 7.5,
+                                    justifyContent: 'center',
+                                    '--joy-focus-outlineOffset': '4px',
+                                    '--joy-palette-focusVisible': (theme) =>
+                                        theme.vars.palette.neutral
+                                            .outlinedBorder,
+                                    [`& .${radioClasses.checked}`]: {
+                                        [`& .${radioClasses.label}`]: {
+                                            fontWeight: 'lg',
+                                        },
+                                        [`& .${radioClasses.action}`]: {
+                                            '--variant-borderWidth': '1px',
+                                            borderColor: 'text.secondary',
+                                        },
                                     },
-                                    [`& .${radioClasses.action}`]: {
-                                        '--variant-borderWidth': '1px',
-                                        borderColor: 'text.secondary',
-                                    },
-                                },
-                                [`& .${radioClasses.action}.${radioClasses.focusVisible}`]:
-                                    {
-                                        outlineWidth: '2px',
-                                    },
-                            }}
-                        >
-                            <TooltipImproved
-                                text={
-                                    usersFilter === 'ALL'
-                                        ? 'All Employees'
-                                        : usersFilter === 'W ASSETS'
-                                          ? 'Employees with Assets'
-                                          : 'Employees without Assets'
-                                }
-                                placement="top"
-                                offset={[0, 5]}
+                                    [`& .${radioClasses.action}.${radioClasses.focusVisible}`]:
+                                        {
+                                            outlineWidth: '2px',
+                                        },
+                                }}
                             >
-                                <Radio
-                                    color="neutral"
-                                    overlay
-                                    disableIcon
-                                    value={
+                                <TooltipImproved
+                                    text={
                                         usersFilter === 'ALL'
-                                            ? 'all'
+                                            ? 'All Employees'
                                             : usersFilter === 'W ASSETS'
-                                              ? 'with'
-                                              : 'without'
+                                              ? 'Employees with Assets'
+                                              : 'Employees without Assets'
                                     }
-                                    label={usersFilter}
-                                />
-                            </TooltipImproved>
-                        </Sheet>
-                    ))}
-                </RadioGroup>
-            </Box>
+                                    placement="top"
+                                    offset={[0, 5]}
+                                >
+                                    <Radio
+                                        color="neutral"
+                                        overlay
+                                        disableIcon
+                                        value={
+                                            usersFilter === 'ALL'
+                                                ? 'all'
+                                                : usersFilter === 'W ASSETS'
+                                                  ? 'with'
+                                                  : 'without'
+                                        }
+                                        label={usersFilter}
+                                    />
+                                </TooltipImproved>
+                            </Sheet>
+                        ))}
+                    </RadioGroup>
+                </Box>
+            </CssVarsProvider>
         </div>
     )
 }
